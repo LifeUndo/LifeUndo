2f7ea17fc58c8257b1b3442f1b0f65937a1e1f7f
LifeUndo <developer@lifeundo.ru>
2025-10-03 05:00:02 +0300

    fix(revalidate): remove revalidate from client page; keep only in server layout

--- src/app/[locale]/downloads/page.tsx
+++ src/app/[locale]/downloads/page.tsx
@@ -3,7 +3,6 @@
 import DownloadsClient from './DownloadsClient';
 
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
 
 export default function Page() {
   return <DownloadsClient />;
